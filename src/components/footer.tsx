aca86059e5e048570808a7bc20d3e4dadf9bc820
Krzysztof Płonka <57814080+chris-plonka@users.noreply.github.com>
2026-05-27 19:31:43 +0200

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className='mx-auto max-w-4xl px-6'>
       <div className='flex h-footer items-center justify-between'>
-        <p className='text-sm'>© {new Date().getFullYear()} Nelson Lai</p>
+        <p className='text-sm'>© {new Date().getFullYear()}</p>
       </div>
     </footer>
   )
